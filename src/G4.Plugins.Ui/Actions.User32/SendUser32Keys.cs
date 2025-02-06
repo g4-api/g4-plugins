af533ab468f8145e181934395d7a85e53a92475f
gravity-api <gravity.api@outlook.com>
2025-02-06 12:57:19 +0200

    Add SendUser32Keys plugin for keyboard input automation
    Added a JSON configuration file `SendUser32Keys.json` for the
    `SendUser32Keys` plugin, including metadata and usage examples.
    Introduced the `SendUser32Keys` class in `SendUser32Keys.cs`
    within the `G4.Plugins.Ui.Actions.User32` namespace, decorated
    with the `[G4Plugin]` attribute. Implemented the `OnSend` method
    to handle sending keys to a UI element using the Windows User32
    API, with support for WebDriver instances and configurable delays.

---
+++ src/G4.Plugins.Ui/Actions.User32/SendUser32Keys.cs
@@ -0,0 +1,56 @@
+﻿using G4.Attributes;
+using G4.Extensions;
+using G4.Models;
+using G4.WebDriver.Remote.Uia;
+
+using System;
+
+namespace G4.Plugins.Ui.Actions.User32
+{
+    [G4Plugin(
+        assembly: "G4.Plugins.Ui, Version=8.0.0.0, Culture=neutral, PublicKeyToken=null",
+        manifest: $"G4.Plugins.Ui.Actions.User32.Manifests.{nameof(SendUser32Keys)}.json")]
+    public class SendUser32Keys(G4PluginSetupModel pluginSetup) : PluginBase(pluginSetup)
+    {
+        protected override PluginResponseModel OnSend(PluginDataModel pluginData)
+        {
+            // Get the WebDriver instance from the plugin data and check if it is a UiaDriver.
+            // Exit the plugin if the WebDriver is not a UiaDriver.
+            if (WebDriver is not IUser32Driver driver)
+            {
+                return this.NewPluginResponse();
+            }
+
+            // Check if 'Keys' argument is present in plugin data and extract the value.
+            var keys = pluginData.Parameters.Get(key: "Keys", defaultValue: string.Empty);
+
+            // Determine the keys to send based on the availability of 'Keys' argument.
+            var text = !string.IsNullOrEmpty(keys)
+                ? keys
+                : pluginData.Rule.Argument;
+
+            // Get the delay data or use the default search timeout as a fallback
+            var delayData = pluginData.Parameters.Get("Delay", "0");
+            var delay = delayData.ConvertToTimeSpan(defaultValue: TimeSpan.Zero);
+
+            // Get the element using provided plugin data.
+            var element = this.GetUser32Element(pluginData);
+
+            // If the element found, set focus to the element.
+            try
+            {
+                element?.SetFocus();
+            }
+            catch
+            {
+                // Silently ignore the exception.
+            }
+
+            // Send the keys to the element.
+            driver.SendKeys(text, delay);
+
+            // Return new plugin response.
+            return this.NewPluginResponse();
+        }
+    }
+}
