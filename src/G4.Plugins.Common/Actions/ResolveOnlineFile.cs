6837ef10d6d4898d2a9b93321eb8706f4e68aa09
gravity-api <gravity.api@outlook.com>
2025-05-29 21:06:09 +0300

    Enhance ResolveOnlineFile plugin and documentation
    Updated `ResolveOnlineFile.json` with a complete overhaul of descriptions, key features, and usage examples, focusing on file retrieval and metadata management.
    
    In `ResolveOnlineFile.cs`, introduced `CleanSessionParameters` for better session management, refactored URI retrieval into `GetUri` with regex support, and improved HTTP request handling for enhanced error management. Added support for full file name in session parameters.

--- src/G4.Plugins.Common/Actions/ResolveOnlineFile.cs
+++ src/G4.Plugins.Common/Actions/ResolveOnlineFile.cs
@@ -1,11 +1,14 @@
 ﻿using G4.Attributes;
 using G4.Extensions;
 using G4.Models;
+using G4.Plugins.Engine;
+using G4.WebDriver.Remote;
 
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 
 namespace G4.Plugins.Common.Actions
 {
@@ -23,8 +26,14 @@ namespace G4.Plugins.Common.Actions
 
             try
             {
+                // Get the web element based on the rule and element in the plugin data.
+                var element = this.GetElement(pluginData.Rule, pluginData.Element);
+
+                // Get the URL to navigate to.
+                var resolvedUri = GetUri(pluginData.Rule, element);
+
                 // Parse the URI from the rule argument (must be a valid absolute URI)
-                uri = new Uri(pluginData.Rule.Argument);
+                uri = new Uri(resolvedUri);
 
                 // Prepare an HTTP GET request for the parsed URI
                 request = new HttpRequestMessage(HttpMethod.Get, uri);
@@ -35,12 +44,7 @@ namespace G4.Plugins.Common.Actions
             catch (Exception e)
             {
                 // Remove any previously set session parameters related to file resolution
-                Invoker.Context.SessionParameters.TryRemove(key: "ResolvedFileData", out _);
-                Invoker.Context.SessionParameters.TryRemove(key: "ResolvedFileExtension", out _);
-                Invoker.Context.SessionParameters.TryRemove(key: "ResolvedFileName", out _);
-                Invoker.Context.SessionParameters.TryRemove(key: "ResolvedFileSize", out _);
-                Invoker.Context.SessionParameters.TryRemove(key: "ResolvedFileStatusCode", out _);
-                Invoker.Context.SessionParameters.TryRemove(key: "ResolvedFileUri", out _);
+                CleanSessionParameters(Invoker);
 
                 // If an exception occurs while sending the request, log it as a G4ExceptionModel.
                 Exceptions.Add(new G4ExceptionModel(rule: pluginData.Rule, exception: e.GetBaseException()));
@@ -74,14 +78,6 @@ namespace G4.Plugins.Common.Actions
             // Determine the file size in bytes
             var size = bytes.LongLength;
 
-            // Store the processed values in session parameters (keys in alphabetical order)
-            Invoker.Context.SessionParameters["ResolvedFileData"] = data.ConvertToBase64();
-            Invoker.Context.SessionParameters["ResolvedFileExtension"] = extension.ConvertToBase64();
-            Invoker.Context.SessionParameters["ResolvedFileName"] = name.ConvertToBase64();
-            Invoker.Context.SessionParameters["ResolvedFileSize"] = size;
-            Invoker.Context.SessionParameters["ResolvedFileStatusCode"] = statusCode;
-            Invoker.Context.SessionParameters["ResolvedFileUri"] = uri.AbsoluteUri.ConvertToBase64();
-
             // Return the populated response back to the caller
             return NewPluginResponse(
                 plugin: this,
@@ -94,12 +90,58 @@ namespace G4.Plugins.Common.Actions
                 uri: uri.AbsoluteUri);
         }
 
+        // Cleans up session parameters related to file resolution.
+        private static void CleanSessionParameters(IAutomationInvoker invoker)
+        {
+            // Remove any previously set session parameters related to file resolution
+            invoker.Context.SessionParameters.TryRemove(key: "ResolvedFileData", out _);
+            invoker.Context.SessionParameters.TryRemove(key: "ResolvedFileExtension", out _);
+            invoker.Context.SessionParameters.TryRemove(key: "ResolvedFileFullName", out _);
+            invoker.Context.SessionParameters.TryRemove(key: "ResolvedFileName", out _);
+            invoker.Context.SessionParameters.TryRemove(key: "ResolvedFileSize", out _);
+            invoker.Context.SessionParameters.TryRemove(key: "ResolvedFileStatusCode", out _);
+            invoker.Context.SessionParameters.TryRemove(key: "ResolvedFileUri", out _);
+        }
+
+        // Gets the URI based on the provided action rule and web element.
+        private static string GetUri(G4RuleModelBase rule, IWebElement element)
+        {
+            // If the element is not found, return the URL from the rule argument.
+            if (element == default)
+            {
+                return rule.Argument;
+            }
+
+            // If OnAttribute is not specified, use the element's text content as the URL.
+            // Otherwise, get the URL from the specified attribute.
+            var url = string.IsNullOrEmpty(rule.OnAttribute)
+                ? element.Text
+                : element.GetAttribute(rule.OnAttribute);
+
+            // Use regular expression to match and extract the desired part of the URL.
+            var match = Regex.Match(
+                input: url,
+                pattern: rule.RegularExpression);
+
+            // Return the desired part of the URL.
+            return match.Value;
+        }
+
         // Creates a new PluginResponseModel with the provided parameters.
         private static PluginResponseModel NewPluginResponse(
             PluginBase plugin, string data, string extension, string fullName, string name, long size, int statusCode, string uri)
         {
             // Build the plugin response
             var pluginResponse = plugin.NewPluginResponse();
+
+            // Store the processed values in session parameters (keys in alphabetical order)
+            plugin.Invoker.Context.SessionParameters["ResolvedFileData"] = data.ConvertToBase64();
+            plugin.Invoker.Context.SessionParameters["ResolvedFileExtension"] = extension.ConvertToBase64();
+            plugin.Invoker.Context.SessionParameters["ResolvedFileFullName"] = fullName;
+            plugin.Invoker.Context.SessionParameters["ResolvedFileName"] = name.ConvertToBase64();
+            plugin.Invoker.Context.SessionParameters["ResolvedFileSize"] = size;
+            plugin.Invoker.Context.SessionParameters["ResolvedFileStatusCode"] = statusCode;
+            plugin.Invoker.Context.SessionParameters["ResolvedFileUri"] = uri.ConvertToBase64();
 
             // Populate the Entity dictionary with the provided parameters
             pluginResponse.Entity = new Dictionary<string, object>
