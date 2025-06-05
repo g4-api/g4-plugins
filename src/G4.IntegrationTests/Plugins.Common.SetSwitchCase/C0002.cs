af87589b9fc49a5140b0edd2a087d78e44d4ff2b
gravity-api <gravity.api@outlook.com>
2025-06-05 21:34:00 +0300

    Refactor C0002.cs for integration testing enhancements
    Updated C0002.cs to include necessary using directives and
    removed unused namespaces. The class now inherits from
    TestCaseBase and accepts a TestContext parameter. Added
    the OnActions method to return a collection of G4RuleModelBase
    objects, implementing a SwitchRuleModel with multiple branches
    for registering parameters. Included an assertion to verify
    the TestResult parameter equals 3 using the Assert plugin.

--- src/G4.IntegrationTests/Plugins.Common.SetSwitchCase/C0002.cs
+++ src/G4.IntegrationTests/Plugins.Common.SetSwitchCase/C0002.cs
@@ -1,12 +1,66 @@
-﻿using System;
+﻿using G4.IntegrationTests.Framework;
+using G4.Models;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace G4.IntegrationTests.Plugins.Common.SetSwitchCase
 {
-    internal class C0002
+    internal class C0002(TestContext context) : TestCaseBase(context)
     {
+        protected override IEnumerable<G4RuleModelBase> OnActions(AutomationEnvironment environment)
+        {
+            return
+            [
+                new SwitchRuleModel
+                {
+                    PluginName = "SetSwitchCase",
+                    Locator = "Id",
+                    OnElement = "ElementText",
+                    Branches = new Dictionary<string, IEnumerable<G4RuleModelBase>>()
+                    {
+                        ["1"] =
+                            [
+                                new ActionRuleModel
+                                {
+                                    PluginName = "RegisterParameter",
+                                    Argument = "{{$ --Name:TestResult --Value:1 --Scope:Session}}"
+                                }
+                            ],
+                        ["2"] =
+                            [
+                                new ActionRuleModel
+                                {
+                                    PluginName = "RegisterParameter",
+                                    Argument = "{{$ --Name:TestResult --Value:2 --Scope:Session}}"
+                                }
+                            ],
+                        ["ElementText"] =
+                            [
+                                new ActionRuleModel
+                                {
+                                    PluginName = "RegisterParameter",
+                                    Argument = "{{$ --Name:TestResult --Value:3 --Scope:Session}}"
+                                }
+                            ],
+                        ["4"] =
+                            [
+                                new ActionRuleModel
+                                {
+                                    PluginName = "RegisterParameter",
+                                    Argument = "{{$ --Name:TestResult --Value:4 --Scope:Session}}"
+                                }
+                            ]
+                    }
+                },
+                new ActionRuleModel
+                {
+                    PluginName = "Assert",
+                    Argument = "{{$ --Condition:Text --Operator:Eq --Expected:3}}",
+                    OnElement = "{{$Get-Parameter --Name:TestResult --Scope:Session}}"
+                }
+            ];
+        }
     }
 }
